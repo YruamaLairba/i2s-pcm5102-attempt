0c9eee7574b24a20f0f29a15b80e224150d19f6b
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-12-09 22:20:04 +0100

    some cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-//use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
-
 use crate::hal::{pac, prelude::*};
 use core::panic::PanicInfo;
 use cortex_m_rt::entry;
@@ -15,14 +8,20 @@ use pac::interrupt;
 use rtt_target::{rprintln, rtt_init_print};
 use stm32f4xx_hal as hal;
 
+//PLLI2S clock configuration
 const PLLI2SM: u8 = 4;
 const PLLI2SN: u16 = 192;
 const PLLI2SR: u8 = 5;
 
+//Clock configuration of the used i2s interface
 const I2SDIV: u8 = 12;
 const ODD: bool = true;
 
-//const MCK_USE: bool = false;
+//generate Master Clock ? Modifying this require to adapt the i2s clock
+const MCK:bool = false;
+
+//Periode of Sawtooth in number of sample
+const SAWTOOTH_PERIOD: u16 = 48_000 / 110;
 
 #[entry]
 fn main() -> ! {
@@ -60,14 +59,15 @@ fn main() -> ! {
         });
         //run the clock
         rcc.cr.modify(|_, w| w.plli2son().set_bit());
-        //wait a stable clock
+        //wait for a stable clock
         while rcc.cr.read().plli2srdy().bit_is_clear() {}
     }
-    //i2s gpio
-    //  SD pb15,pc3
-    //  WS pb9, pb12
-    //  CK pb10,pb13,pc7,*pd3
-    //  MCK pa3, pa6, pc6,
+    //i2s2 gpio
+    //Note, on nucleo board possible i2s2 gpio are:
+    //  SD: pb15, pc3
+    //  WS: pb9, pb12
+    //  CK: pb10, pb13, pc7
+    //  MCK: pa3, pa6, pc6
 
     let _pb15 = gpiob.pb15.into_alternate_af5(); //SD DIN
     let _pb12 = gpiob.pb12.into_alternate_af5(); //WS LRCK
@@ -85,9 +85,8 @@ fn main() -> ! {
     //Spi2 setup for i2s mode
     unsafe {
         let spi2 = &(*pac::SPI2::ptr());
-        spi2.i2spr.modify(|_, w| {
-                w.i2sdiv().bits(I2SDIV).odd().bit(ODD).mckoe().disabled()
-        });
+        spi2.i2spr
+            .modify(|_, w| w.i2sdiv().bits(I2SDIV).odd().bit(ODD).mckoe().bit(MCK));
         spi2.i2scfgr.modify(|_, w| {
             w.i2smod()
                 .i2smode() //
@@ -107,17 +106,11 @@ fn main() -> ! {
                 .enabled() //start i2S
         })
     }
-    rprintln!("init done");
-    //check spi2 status
-    unsafe {
-        let spi2 = &(*pac::SPI2::ptr());
-        let spi2_sr = *((pac::SPI2::ptr() as usize + 0x08) as *const u32);
-        rprintln!("{:#032b} {}", spi2_sr, spi2.sr.read().txe().bit());
-    }
 
     loop {
-        for spl_n in 0..100 {
-            let spl = spl_n * ((i16::MAX - i16::MAX / 2) / 100);
+        for spl_n in 0..SAWTOOTH_PERIOD as i16 {
+            let spl: i16 = (spl_n - (SAWTOOTH_PERIOD as i16) / 2)
+                * (i16::MAX / ((SAWTOOTH_PERIOD as i16) / 2));
             let l = spl;
             let r = spl;
 
@@ -125,26 +118,16 @@ fn main() -> ! {
                 let spi2 = &(*pac::SPI2::ptr());
                 while !spi2.sr.read().txe().bit() {}
                 spi2.dr.modify(|_, w| w.dr().bits(l as u16));
-                // i2s_sr_check();
-                // while !spi2.sr.read().txe().bit() {}
-                // spi2.dr.modify(|_,w| w.dr().bits((l & 0x00FF) as u16));
-                // i2s_sr_check();
                 while !spi2.sr.read().txe().bit() {}
                 spi2.dr.modify(|_, w| w.dr().bits(r as u16));
-                // i2s_sr_check();
-                // while !spi2.sr.read().txe().bit() {}
-                // spi2.dr.modify(|_,w| w.dr().bits((r & 0x00FF) as u16));
-                // i2s_sr_check();
             }
         }
-        // your code goes here
     }
 }
 
 #[interrupt]
 fn SPI2() {
-    static mut COUNT: i16 = 0;
-    const COUNT_MAX: i16 = 20;
+    static mut SPL_N: u16 = 0;
     unsafe {
         let spi2 = &(*pac::SPI2::ptr());
         if spi2.sr.read().fre().bit() {
@@ -158,13 +141,14 @@ fn SPI2() {
         }
         if spi2.sr.read().txe().bit() {
             let side = spi2.sr.read().chside().bit();
-            let spl = *COUNT * ((i16::MAX - i16::MAX / 2) / 20);
+            let spl: i16 = (*SPL_N as i16 - (SAWTOOTH_PERIOD as i16) / 2)
+                * (i16::MAX / ((SAWTOOTH_PERIOD as i16) / 2));
 
             spi2.dr.modify(|_, w| w.dr().bits(spl as u16));
             if side {
-                *COUNT += 1;
-                if *COUNT == COUNT_MAX {
-                    *COUNT = 0;
+                *SPL_N+= 1;
+                if *SPL_N == SAWTOOTH_PERIOD {
+                    *SPL_N = 0;
                 }
             }
         }
